ca1c879b48dbab98380772bfca2c4656dd6c0370
PieroTicona <piero.ticona.software@gmail.com>
2026-05-02 16:29:54 -0500

    refactor: sdebar primeng

--- src/app/pages/inventory/inventory.module.ts
+++ src/app/pages/inventory/inventory.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { InventoryComponent } from './inventory/inventory.component';
+import {SliderModule} from "primeng/slider";
 
 @NgModule({
   declarations: [InventoryComponent],
-  imports: [CommonModule, FormsModule, InventoryRoutingModule]
+  imports: [CommonModule, FormsModule, CheckboxModule, InventoryRoutingModule, SliderModule]
 })
 export class InventoryModule { }
