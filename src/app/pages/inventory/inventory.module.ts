08d79d0139779e6bd01fa3bd53cb4102319f9d16
PieroTicona <piero.ticona.software@gmail.com>
2026-05-03 21:57:38 -0500

    Add primeng invetory

--- src/app/pages/inventory/inventory.module.ts
+++ src/app/pages/inventory/inventory.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox';
+import { SliderModule } from 'primeng/slider';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { InventoryRoutingModule } from './inventory-routing.module';
 import { InventoryComponent } from './inventory/inventory.component';
-import {SliderModule} from "primeng/slider";
 
 @NgModule({
   declarations: [InventoryComponent],
-  imports: [CommonModule, FormsModule, CheckboxModule, InventoryRoutingModule, SliderModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    CheckboxModule,
+    SliderModule,
+    InputTextModule,
+    FloatLabelModule,
+    ButtonModule,
+    CardModule,
+    InventoryRoutingModule,
+  ]
 })
 export class InventoryModule { }
